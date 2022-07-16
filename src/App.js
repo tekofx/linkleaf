95fdc7b5cdacd168b2d4dbe75f1c9cab51f9d3c8
tekofx <tekofxx@gmail.com>
2022-07-16 13:38:55 +0200

    First files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import SocialButton from "./components/Button";
+import { Container, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <Container>
+      <SocialButton />
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Grid } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
+
+export default function SocialButton() {
+  return <Button>SocialButton</Button>;
+}
