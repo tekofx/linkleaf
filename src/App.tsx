4821dd7c9532aa85a74d5c54858d88d61e73f1e9
tekofx <tekofxx@gmail.com>
2025-10-14 16:16:30 +0200

    Added base code

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import av from "./avatar.jpg";
+import {
+  Container,
+  Typography,
+  ThemeProvider,
+  Grid,
+  Avatar,
+} from "@mui/material";
+import data from "./data.json";
+import GithubCorner from "react-github-corner";
+import LinkIcon from "@mui/icons-material/Link";
+import Theme from "./theme";
+import AnimatedButton from "./components/AnimatedButton";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={Theme}>
+      <GithubCorner href="https://github.com/tekofx/linkleaf" target="_blank" />
+      <Container maxWidth="lg">
+        <Grid container justifySelf="center">
+          <Grid alignSelf="center" size={12}>
+            <Avatar src={av} sx={{ width: 150, height: "auto" }} />
+          </Grid>
+          <Grid alignSelf="center" size={12} sx={{ marginBottom: 1 }}>
+            <Typography variant="username">{data.username}</Typography>
+          </Grid>
+          <Grid alignSelf="center" size={12} sx={{ marginBottom: 1 }}>
+            <Typography variant="description">{data.description}</Typography>
+          </Grid>
+
+          <Grid
+            alignSelf="center"
+            size={12}
+            sx={{
+              marginBottom: 2,
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <a href={data.web}>
+              <LinkIcon />
+            </a>
+            <a href={data.web}>
+              <Typography variant="web">{data.web}</Typography>
+            </a>
+          </Grid>
+          <Grid>
+            <br />
+          </Grid>
+
+          {data.links.map((link) => (
+            <Grid alignSelf="center" size={{ xs: 12, sm: 6, md: 6, lg: 4 }}>
+              <AnimatedButton
+                name={link.name}
+                link={link.link}
+                icon={link.icon}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/AnimatedButton.tsx
@@ -0,0 +1,70 @@
+import { useSpring, animated } from "react-spring";
+import { useState } from "react";
+import { Card, Icon, Grid } from "@mui/material";
+
+import Theme from "../theme";
+
+interface AnimatedButtonProps {
+  name: string;
+  link: string;
+  icon: string;
+}
+
+export default function AnimatedButton({
+  name,
+  link,
+  icon,
+}: AnimatedButtonProps) {
+  const [state, toggle] = useState(true);
+  const AnimatedCard = animated(Card);
+
+  const styles = useSpring({
+    loop: false,
+    from: { scale: 1, color: "white", bgcolor: Theme.palette.background.paper },
+    to: {
+      scale: state ? 1 : 1.05,
+      color: state ? "white" : "black",
+      bgcolor: state ? "white" : Theme.palette.background.paper,
+    },
+    config: { duration: 200 },
+  });
+
+  return (
+    <>
+      <AnimatedCard
+        onMouseEnter={() => toggle(!state)}
+        onMouseLeave={() => toggle(!state)}
+        onClick={() => window.open(link)}
+        style={styles}
+        sx={{
+          bgcolor: styles.bgcolor.animation.to,
+          padding: 2,
+          width: "70%",
+        }}
+      >
+        <Grid container spacing={6}>
+          <Grid
+            item
+            xs={2}
+            sm={2}
+            md={2}
+            lg={2}
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+            }}
+          >
+            <Icon className={icon} />
+          </Grid>
+          <Grid item xs={8} sm={8} md={8} lg={8}>
+            <b>
+              <p style={{ padding: 0, margin: 0 }}>{name}</p>
+            </b>
+          </Grid>
+        </Grid>
+      </AnimatedCard>
+      <br />
+    </>
+  );
+}
