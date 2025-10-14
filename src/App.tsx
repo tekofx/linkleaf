c9379f2143deba81fe3f237107078a37a39bae58
tekofx <tekofxx@gmail.com>
2025-10-14 16:30:25 +0200

    Added typographies and fixed layout

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,11 @@ import {
   ThemeProvider,
   Grid,
   Avatar,
+  Stack,
 } from "@mui/material";
 import data from "./data.json";
+import { useEffect } from "react";
+import { loadCSS } from "fg-loadcss";
 import GithubCorner from "react-github-corner";
 import LinkIcon from "@mui/icons-material/Link";
 import Theme from "./theme";
@@ -17,38 +20,24 @@ function App() {
     <ThemeProvider theme={Theme}>
       <GithubCorner href="https://github.com/tekofx/linkleaf" target="_blank" />
       <Container maxWidth="lg">
-        <Grid container justifySelf="center">
-          <Grid alignSelf="center" size={12}>
-            <Avatar src={av} sx={{ width: 150, height: "auto" }} />
-          </Grid>
-          <Grid alignSelf="center" size={12} sx={{ marginBottom: 1 }}>
-            <Typography variant="username">{data.username}</Typography>
-          </Grid>
-          <Grid alignSelf="center" size={12} sx={{ marginBottom: 1 }}>
-            <Typography variant="description">{data.description}</Typography>
-          </Grid>
+        <Stack
+          sx={{
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Avatar src={av} sx={{ width: 150, height: "auto" }} />
+          <Typography variant="username">{data.username}</Typography>
+          <Typography variant="description">{data.description}</Typography>
 
-          <Grid
-            alignSelf="center"
-            size={12}
-            sx={{
-              marginBottom: 2,
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <a href={data.web}>
-              <LinkIcon />
-            </a>
-            <a href={data.web}>
-              <Typography variant="web">{data.web}</Typography>
-            </a>
-          </Grid>
-          <Grid>
-            <br />
-          </Grid>
-
+          <a href={data.web}>
+            <LinkIcon />
+          </a>
+          <a href={data.web}>
+            <Typography variant="web">{data.web}</Typography>
+          </a>
+        </Stack>
+        <Grid container>
           {data.links.map((link) => (
             <Grid alignSelf="center" size={{ xs: 12, sm: 6, md: 6, lg: 4 }}>
               <AnimatedButton
